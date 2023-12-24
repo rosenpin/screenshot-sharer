30dd8e176e71a6511a66c5cf0f1b6918437bbc87
rosenpin <mail@tomerrosenfeld.com>
2023-12-24 20:23:13 +0100

    fixes for android 11+

--- app/src/main/java/com/tomer/screenshotsharer/AssistLoggerSession.kt
+++ app/src/main/java/com/tomer/screenshotsharer/AssistLoggerSession.kt
@@ -1,50 +1,30 @@
 package com.tomer.screenshotsharer
 
 import android.animation.Animator
-import android.app.assist.AssistContent
-import android.app.assist.AssistStructure
 import android.content.ContentValues
 import android.content.Context
-import android.content.Context.WINDOW_SERVICE
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.graphics.PixelFormat
 import android.media.MediaScannerConnection
 import android.net.Uri
-import android.os.Build
-import android.os.Bundle
 import android.os.Environment
-import android.os.Handler
 import android.provider.MediaStore
 import android.provider.Settings
 import android.service.voice.VoiceInteractionSession
 import android.util.Log
-import android.view.ViewGroup
-import android.view.WindowManager
+import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
-import java.io.File
-import java.io.FileOutputStream
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
+import java.util.Locale
+
 
 class AssistLoggerSession(context: Context) : VoiceInteractionSession(context) {
 
     override fun onHandleAssist(state: AssistState) {
         super.onHandleAssist(state)
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
-            Log.d("Data", state.assistData.toString())
-        }
-    }
-
-    override fun onHandleAssist(
-        data: Bundle?,
-        structure: AssistStructure?,
-        content: AssistContent?
-    ) {
-        super.onHandleAssist(data, structure, content)
-        Log.d("Data", data.toString())
+        Log.d("Data", state.assistData.toString())
     }
 
     override fun onHandleScreenshot(screenshot: Bitmap?) {
@@ -59,111 +39,84 @@ class AssistLoggerSession(context: Context) : VoiceInteractionSession(context) {
     }
 
     private fun showPreviewAndFinish(screenshot: Bitmap?) {
-        val windowManager = context.getSystemService(WINDOW_SERVICE) as WindowManager
-        val rootView = layoutInflater.inflate(R.layout.image_view, null)
-        val imageView = rootView.findViewById(R.id.image_view) as ImageView
-        imageView.alpha = 0.0f
-        imageView.setImageBitmap(screenshot)
-        val type =
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
-            else WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
-
-        val params = WindowManager.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            type,
-            0,
-            PixelFormat.TRANSLUCENT
-        )
-        windowManager.addView(rootView, params)
-        Handler().post {
-            imageView.animate().alpha(1.0f).setDuration(700)
-                .setListener(object : Animator.AnimatorListener {
-                    override fun onAnimationStart(animation: Animator) {
-
-                    }
-
-                    override fun onAnimationEnd(animation: Animator) {
-                        shareBitmap(screenshot)
-                        windowManager.removeView(rootView)
-                        finish()
-                    }
-
-                    override fun onAnimationCancel(animation: Animator) {
-
-                    }
-
-                    override fun onAnimationRepeat(animation: Animator) {
-
-                    }
-                })
+        if (screenshot == null) {
+            Toast.makeText(context, "Screenshot is null", Toast.LENGTH_SHORT).show()
+            finish()
+            return
         }
+        setUiEnabled(true)
+
+        val rootView = layoutInflater.inflate(R.layout.image_view, null)
+        rootView.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+        val imageView = rootView.findViewById(R.id.image_view) as ImageView
+        imageView.setImageBitmap(screenshot)
+
+        Log.d("Screenshot", "Showing preview")
+        setContentView(rootView)
+        imageView.alpha = 0.0f
+        imageView.animate().alpha(1.0f).setDuration(500)
+            .setListener(object : Animator.AnimatorListener {
+                override fun onAnimationStart(animation: Animator) {
+
+                }
+
+                override fun onAnimationEnd(animation: Animator) {
+                    shareBitmap(screenshot)
+                    finish()
+                }
+
+                override fun onAnimationCancel(animation: Animator) {
+
+                }
+
+                override fun onAnimationRepeat(animation: Animator) {
+
+                }
+            })
     }
 
     private fun saveImage(bitmap: Bitmap?, fileName: String): String? {
         if (bitmap == null)
             return null
-
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
-            val contentValues = ContentValues().apply {
-                put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
-                put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-                put(
-                    MediaStore.MediaColumns.RELATIVE_PATH,
-                    Environment.DIRECTORY_PICTURES + "/Screenshots"
-                )
-            }
-            val uri = context.contentResolver.insert(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                contentValues
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            put(
+                MediaStore.MediaColumns.RELATIVE_PATH,
+                Environment.DIRECTORY_PICTURES + "/Screenshots"
             )
-            if (uri != null) {
-                context.contentResolver.openOutputStream(uri).use {
-                    if (it == null)
-                        return@use
+        }
+        val uri = context.contentResolver.insert(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        )
+        uri?.let {
+            context.contentResolver.openOutputStream(it).use { outputStream ->
+                outputStream?.let { stream ->
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 85, stream)
+                    stream.flush()
+                    stream.close()
 
-                    bitmap.compress(Bitmap.CompressFormat.PNG, 85, it)
-                    it.flush()
-                    it.close()
-
-                    // add pic to gallery
+                    // Add pic to gallery
                     MediaScannerConnection.scanFile(context, arrayOf(uri.toString()), null, null)
                 }
             }
             return uri.toString()
         }
 
-        val filePath = Environment.getExternalStoragePublicDirectory(
-            Environment.DIRECTORY_PICTURES + "/Screenshots"
-        ).absolutePath
-
-        Toast.makeText(this.context, filePath, Toast.LENGTH_LONG).show()
-        val dir = File(filePath)
-        if (!dir.exists()) dir.mkdirs()
-        val file = File(dir, fileName)
-        val fOut = FileOutputStream(file)
-
-        bitmap.compress(Bitmap.CompressFormat.PNG, 85, fOut)
-        fOut.flush()
-        fOut.close()
-
-        // add pic to gallery
-        MediaScannerConnection.scanFile(context, arrayOf(file.toString()), null, null)
-        return filePath
+        return null
     }
 
     private fun shareBitmap(bitmap: Bitmap?) {
-        if (!canWriteExternalPermission()) {
-            val mainActivity = Intent(context, MainActivity::class.java)
-            mainActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            context.startActivity(mainActivity)
-            return
-        }
         val time = SimpleDateFormat("yyyy-MM-dd_HH_mm_ss", Locale.getDefault()).format(Date())
         val fileName = "screenshot-$time.png"
 
-        val path = saveImage(bitmap, fileName) ?: return
+        val path = saveImage(bitmap, fileName) ?: run {
+            Log.e("Screenshot", "Failed to save image")
+            return
+        }
+
+        Log.d("Screenshot", "Saved image to $path")
 
         val bmpUri = Uri.parse(path)
         val shareIntent = Intent(Intent.ACTION_SEND)
@@ -171,15 +124,9 @@ class AssistLoggerSession(context: Context) : VoiceInteractionSession(context) {
         shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri)
         shareIntent.putExtra(Intent.EXTRA_TEXT, "")
         shareIntent.type = "image/jpeg"
-        val finalShareIntent =
-            Intent.createChooser(shareIntent, "Select the app you want to share the screenshot to")
+        val finalShareIntent = Intent.createChooser(shareIntent, "Share screenshot to")
         finalShareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        Log.d("Screenshot", "Sharing image")
         context.startActivity(finalShareIntent)
-    }
-
-    private fun canWriteExternalPermission(): Boolean {
-        val permission = "android.permission.WRITE_EXTERNAL_STORAGE"
-        val res = context.checkCallingOrSelfPermission(permission)
-        return res == PackageManager.PERMISSION_GRANTED
     }
 }
--- app/src/main/java/com/tomer/screenshotsharer/MainActivity.kt
+++ app/src/main/java/com/tomer/screenshotsharer/MainActivity.kt
@@ -3,7 +3,6 @@ package com.tomer.screenshotsharer
 import android.Manifest
 import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
@@ -17,87 +16,86 @@ import androidx.core.app.ActivityCompat
 
 
 class MainActivity : AppCompatActivity(), View.OnTouchListener {
-	
-	private lateinit var preview: ToggleButton
-	private lateinit var assistant: ToggleButton
-	private lateinit var storage: ToggleButton
-	
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
-		assistant = findViewById<ToggleButton>(R.id.assistant)
-		storage = findViewById<ToggleButton>(R.id.storage)
-		preview = findViewById<ToggleButton>(R.id.preview)
-	}
-	
-	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-		menuInflater.inflate(R.menu.main_menu, menu)
-		menu?.findItem(R.id.github)?.setOnMenuItemClickListener {
-			try {
-				val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/rosenpin/Screenshot-Sharer"))
-				startActivity(intent)
-			} catch (ignored: ActivityNotFoundException) {
-			}
-			true
-		}
-		return super.onCreateOptionsMenu(menu)
-	}
-	
-	override fun onResume() {
-		super.onResume()
-		checkAssistant()
-		checkStorageAccess()
-		checkPreview()
-		assistant.isClickable = false
-		storage.isClickable = false
-		preview.isClickable = false
-	}
-	
-	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-		super.onActivityResult(requestCode, resultCode, data)
-		checkStorageAccess()
-	}
-	
-	private fun checkAssistant() {
-		val currentAssistant = Settings.Secure.getString(contentResolver, "voice_interaction_service")
-		assistant.isChecked = currentAssistant != null && (currentAssistant == packageName + "/." + AssistLoggerService::class.java.simpleName || currentAssistant.contains(packageName))
-		assistant.setOnTouchListener(this)
-	}
-	
-	private fun checkStorageAccess() {
-		if (canWriteExternalPermission())
-			storage.isChecked = true
-		else {
-			storage.isChecked = false
-			storage.setOnTouchListener(this)
-			ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
-		}
-	}
-	
-	private fun checkPreview() {
-		preview.isChecked = Settings.canDrawOverlays(this)
-		preview.setOnTouchListener(this)
-	}
-	
-	private fun canWriteExternalPermission(): Boolean {
-		val permission = Manifest.permission.WRITE_EXTERNAL_STORAGE
-		val res = checkCallingOrSelfPermission(permission)
-		return res == PackageManager.PERMISSION_GRANTED
-	}
-	
-	override fun onTouch(v: View, event: MotionEvent): Boolean {
-		when (v.id) {
-			R.id.assistant -> {
-				startActivity(Intent(Settings.ACTION_VOICE_INPUT_SETTINGS))
-				Toast.makeText(this, "Select " + getString(R.string.app_name) + " as your assist app", Toast.LENGTH_SHORT).show()
-			}
-			R.id.storage -> ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
-			R.id.preview -> {
-				startActivity(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName")))
-				Toast.makeText(this, "Permit drawing over other apps for previews", Toast.LENGTH_SHORT).show()
-			}
-			else -> v.performClick()
-		}
-		return false
-	}
+
+    private lateinit var preview: ToggleButton
+    private lateinit var assistant: ToggleButton
+    private lateinit var storage: ToggleButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        assistant = findViewById<ToggleButton>(R.id.assistant)
+        storage = findViewById<ToggleButton>(R.id.storage)
+        preview = findViewById<ToggleButton>(R.id.preview)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        menu?.findItem(R.id.github)?.setOnMenuItemClickListener {
+            try {
+                val intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://github.com/rosenpin/Screenshot-Sharer")
+                )
+                startActivity(intent)
+            } catch (ignored: ActivityNotFoundException) {
+            }
+            true
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkAssistant()
+        assistant.isClickable = false
+        storage.isClickable = false
+        preview.isClickable = false
+    }
+
+    private fun checkAssistant() {
+        val currentAssistant =
+            Settings.Secure.getString(contentResolver, "voice_interaction_service")
+        assistant.isChecked =
+            currentAssistant != null && (currentAssistant == packageName + "/." + AssistLoggerService::class.java.simpleName || currentAssistant.contains(
+                packageName
+            ))
+        assistant.setOnTouchListener(this)
+    }
+
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        when (v.id) {
+            R.id.assistant -> {
+                startActivity(Intent(Settings.ACTION_VOICE_INPUT_SETTINGS))
+                Toast.makeText(
+                    this,
+                    "Select " + getString(R.string.app_name) + " as your assist app",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            R.id.storage -> ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                1
+            )
+
+            R.id.preview -> {
+                startActivity(
+                    Intent(
+                        Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:$packageName")
+                    )
+                )
+                Toast.makeText(
+                    this,
+                    "Permit drawing over other apps for previews",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            else -> v.performClick()
+        }
+        return false
+    }
 }
